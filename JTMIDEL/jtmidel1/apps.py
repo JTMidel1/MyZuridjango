8d597a71d07f9c82b4f4cbfe02401aff5ed3ea85
JTMidel <100242239+JTMidel1@users.noreply.github.com>
2022-06-05 18:53:55 +0100

    Add files via upload

---
+++ JTMIDEL/jtmidel1/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Jtmidel1Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'jtmidel1'
---
+++ JTMIDEL/jtmidel1/models.py
@@ -0,0 +1,7 @@
+from operator import length_hint
+from unittest.util import _MAX_LENGTH
+from django.db import models
+
+# Create your models here.
+print("Django is a nice framework")
+    
